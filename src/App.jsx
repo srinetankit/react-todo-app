244fcb882293c2e3127aacf01f6e9d88794427b6
Ankit Singh <singhankit0033@gmail.com>
2025-07-01 15:01:40 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Container, Header } from './styles'
+import { TodoProvider } from './context/TodoContext'
+import TodoForm from './components/TodoForm'
 
-function App() {
+const App = () => {
   return (
-    <div>App</div>
+    <TodoProvider>
+        <Container>
+            <Header>TODO APP</Header>
+            <TodoForm />
+        </Container>
+    </TodoProvider>
   )
 }
+
+
+
 
 export default App
