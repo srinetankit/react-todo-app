454740b6076e7fed0b22b3ce4ea1586fdaa50b34
Ankit Singh <singhankit0033@gmail.com>
2025-07-03 23:51:49 +0530

    add functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Header } from './styles'
-import { TodoProvider } from './context/TodoContext'
+import { TodoProvider, useTodoContext } from './context/TodoContext'
 import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+import useLocalStorage from './hooks/useLocalStorage'
 
 const App = () => {
+  const { state, dispatch } = useTodoContext();
+  const [storeData, setStoreData] = useLocalStorage('todos', []);
+
+  useEffect(() => {
+    dispatch({
+      type: "LOAD_TODOS",
+      payload: storeData
+    })
+  }, [])
+
+  useEffect(() => {
+    setStoreData(state.todos)
+  }, [state.todos])
   return (
-    <TodoProvider>
-        <Container>
-            <Header>TODO APP</Header>
-            <TodoForm />
-        </Container>
-    </TodoProvider>
+
+    <Container>
+      <Header>TODO APP</Header>
+      <TodoForm />
+      <TodoList />
+    </Container>
   )
 }
 
